9cbbf863860c91fd0a10eb77db091d04c32ecc59
RipperYayWhatsup <rippearappeared@gmail.com>
2025-09-04 20:59:33 -0400

    Updated StringyNameUL.py

--- StringyNameUL.py
+++ StringyNameUL.py
@@ -1,4 +1,6 @@
 username = "lich liver"
-print(name.upper())
-print(name.lower())
+print(username.upper())
+print(username.lower())
+
 # basically the same function, I'm guessing? Gah, we'll see
+# update, I had to fix btoh of the values, , added user behind name so it functions correctly.
