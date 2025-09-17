b1009faac6b1411ef3e39a7fc7afa14193d203a5
RipperYayWhatsup <rippearappeared@gmail.com>
2025-09-17 19:43:13 -0400

    Some cool thing I made while sleep deprived!

---
+++ PythonApplication2.py
@@ -0,0 +1,18 @@
+lion_user = "Lich"
+didry_user = "Liver"
+user_name = f"{lion_user} {didry_user}"
+
+
+print(user_name)
+
+print("loading..")
+print("...")
+
+first_name = "Johnny"
+last_name = "Bonty"
+
+full_name = f"{first_name} {last_name}"
+
+
+print(f"Hello, {full_name.title()}")
+
